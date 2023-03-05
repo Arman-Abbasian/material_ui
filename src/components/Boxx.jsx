52126fbbc8b7b4036ccdd4f263e49c19129dbb6b
Arman Abbasian <105098083+Arman-Abbasian@users.noreply.github.com>
2023-03-05 09:21:51 +0330

    make Stack component

--- src/components/Boxx.jsx
+++ src/components/Boxx.jsx
@@ -1,8 +1,9 @@
+import { Divider } from "@mui/material";
 import { Box, Stack } from "@mui/system";
 
 const Boxx = () => {
   return (
-    <Stack>
+    <Stack border={'1px dotted blue'} direction='row' spacing={2} flexWrap={"wrap"} divider={<Divider sx={{width:'3px',backgroundColor:'red',marginLeft:0}} orientation="vertical" flexItem />} >
       <Box
         sx={{
           backgroundColor: "peru",
@@ -10,6 +11,30 @@ const Boxx = () => {
           height: "100px",
           width: "100px",
           padding: "4rem",
+          marginLeft:0,
+          "&:hover": { backgroundColor: "red" },
+        }}
+      >
+        hhghhg
+      </Box>
+      <Box
+        width={"100px"}
+        height={"100px"}
+        padding="4rem"
+        color={"red"}
+        marginLeft={0}
+        bgcolor={"green"}
+      >
+        ddddd
+      </Box>
+      <Box
+        sx={{
+          backgroundColor: "peru",
+          color: "white",
+          height: "100px",
+          width: "100px",
+          padding: "4rem",
+          marginLeft:0,
           "&:hover": { backgroundColor: "red" },
         }}
       >
@@ -21,6 +46,7 @@ const Boxx = () => {
         padding="4rem"
         color={"red"}
         bgcolor={"green"}
+        marginLeft={0}
       >
         ddddd
       </Box>
